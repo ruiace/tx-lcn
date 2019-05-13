73ebc0014743123584fe065378e41da7f0bec464
ruipeng <ruiace@126.com>
2019-05-13 22:38:58 +0800

    修改配置文件

--- txlcn-demo-tm/src/main/java/org/txlcn/tm/TransactionManagerApplication.java
+++ txlcn-demo-tm/src/main/java/org/txlcn/tm/TransactionManagerApplication.java
@@ -3,7 +3,6 @@ package org.txlcn.tm;
 import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableTransactionManagerServer
